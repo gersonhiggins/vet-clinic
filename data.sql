27997ba8d7aaafae6c4d6212359f443362a17d73
gersonhiggins <gahg.10gerson@gmail.com>
2023-07-13 15:10:45 -0400

    Requested changes

--- data.sql
+++ data.sql
@@ -61,61 +61,24 @@ INSERT INTO specializations (vet_id, species_id)
 VALUES (4, 1);
 /* set visits */
 INSERT INTO visits (animal_id, vet_id, visit_date)
-VALUES (1, 1, '2020-05-24');
-
-INSERT INTO visits (animal_id, vet_id, visit_date)
-VALUES (1, 2, '2020-07-22');
-
-INSERT INTO visits (animal_id, vet_id, visit_date)
-VALUES (2, 3, '2021-02-02');
-
-INSERT INTO visits (animal_id, vet_id, visit_date)
-VALUES (3, 2, '2020-01-05');
-
-INSERT INTO visits (animal_id, vet_id, visit_date)
-VALUES (3, 2, '2020-03-08');
-
-INSERT INTO visits (animal_id, vet_id, visit_date)
-VALUES (3, 2, '2020-05-14');
-
-INSERT INTO visits (animal_id, vet_id, visit_date)
-VALUES (4, 2, '2021-05-04');
-
-INSERT INTO visits (animal_id, vet_id, visit_date)
-VALUES (5, 3, '2021-02-24');
-
-INSERT INTO visits (animal_id, vet_id, visit_date)
-VALUES (6, 2, '2019-12-21');
-
-INSERT INTO visits (animal_id, vet_id, visit_date)
-VALUES (6, 1, '2020-08-10');
-
-INSERT INTO visits (animal_id, vet_id, visit_date)
-VALUES (6, 2, '2021-04-07');
-
-INSERT INTO visits (animal_id, vet_id, visit_date)
-VALUES (7, 2, '2019-09-29');
-
-INSERT INTO visits (animal_id, vet_id, visit_date)
-VALUES (8, 3, '2020-10-03');
-
-INSERT INTO visits (animal_id, vet_id, visit_date)
-VALUES (8, 3, '2020-11-04');
-
-INSERT INTO visits (animal_id, vet_id, visit_date)
-VALUES (9, 2, '2019-01-24');
-
-INSERT INTO visits (animal_id, vet_id, visit_date)
-VALUES (9, 2, '2019-05-15');
-
-INSERT INTO visits (animal_id, vet_id, visit_date)
-VALUES (9, 2, '2020-02-27');
-
-INSERT INTO visits (animal_id, vet_id, visit_date)
-VALUES (9, 2, '2020-08-03');
-
-INSERT INTO visits (animal_id, vet_id, visit_date)
-VALUES (10, 2, '2020-05-24');
-
-INSERT INTO visits (animal_id, vet_id, visit_date)
-VALUES (10, 1, '2021-01-11');
+VALUES
+    (1, 1, '2020-05-24'), -- Agumon visited William Tatcher on May 24th, 2020
+    (1, 3, '2020-07-22'), -- Agumon visited Stephanie Mendez on Jul 22nd, 2020
+    (2, 4, '2021-02-02'), -- Gabumon visited Jack Harkness on Feb 2nd, 2021
+    (3, 2, '2020-01-05'), -- Pikachu visited Maisy Smith on Jan 5th, 2020
+    (3, 2, '2020-03-08'), -- Pikachu visited Maisy Smith on Mar 8th, 2020
+    (3, 2, '2020-05-14'), -- Pikachu visited Maisy Smith on May 14th, 2020
+    (4, 3, '2021-05-04'), -- Devimon visited Stephanie Mendez on May 4th, 2021
+    (5, 4, '2021-02-24'), -- Charmander visited Jack Harkness on Feb 24th, 2021
+    (6, 2, '2019-12-21'), -- Plantmon visited Maisy Smith on Dec 21st, 2019
+    (6, 1, '2020-08-10'), -- Plantmon visited William Tatcher on Aug 10th, 2020
+    (6, 2, '2021-04-07'), -- Plantmon visited Maisy Smith on Apr 7th, 2021
+    (7, 3, '2019-09-29'), -- Squirtle visited Stephanie Mendez on Sep 29th, 2019
+    (8, 4, '2020-10-03'), -- Angemon visited Jack Harkness on Oct 3rd, 2020
+    (8, 4, '2020-11-04'), -- Angemon visited Jack Harkness on Nov 4th, 2020
+    (9, 2, '2019-01-24'), -- Boarmon visited Maisy Smith on Jan 24th, 2019
+    (9, 2, '2019-05-15'), -- Boarmon visited Maisy Smith on May 15th, 2019
+    (9, 2, '2020-02-27'), -- Boarmon visited Maisy Smith on Feb 27th, 2020
+    (9, 2, '2020-08-03'), -- Boarmon visited Maisy Smith on Aug 3rd, 2020
+    (10, 3, '2020-05-24'), -- Blossom visited Stephanie Mendez on May 24th, 2020
+    (10, 1, '2021-01-11'); -- Blossom visited William Tatcher on Jan 11th, 2021
